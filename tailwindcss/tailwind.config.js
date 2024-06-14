288586847fe0a71bc9fc9deec2939c57919d0598
aloknath1 <nath.alok59@gmail.com>
2024-06-14 10:54:56 +0530

    handson tailwindcss

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{css}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
